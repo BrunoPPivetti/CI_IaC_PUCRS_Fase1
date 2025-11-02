4bf5000246f1a8003bc00b3e799bd7d7a3e95c42
brunoppivetti <brunopivetti2004@gmail.com>
2025-11-02 12:16:36 -0300

    correções no arquivo dos testes e da api

--- test_main.py
+++ test_main.py
@@ -12,3 +12,8 @@ def test_hello():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'Hello':'World'}
+
+def test_devops():
+    response = client.get('pucrs')
+    assert response.status_code == 200
+    assert response.json() == {'fase 1':'pucrs'}
